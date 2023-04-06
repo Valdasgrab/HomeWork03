3edd85f82e8a4e0d09e6e5afd6f65d025691bb2a
vgrabauskas <valdasgrab@gmail.com>
2023-04-06 18:53:30 +0300

    Add listView to activity_main.xml

--- app/src/main/java/lt/vgrabauskas/homework03/MainActivity.kt
+++ app/src/main/java/lt/vgrabauskas/homework03/MainActivity.kt
@@ -2,10 +2,26 @@ package lt.vgrabauskas.homework03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var arrayListView: ListView
+    lateinit var numberArray: Array<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        arrayListView = findViewById(R.id.textListView)
+
+
+        fizzBuzz(numberArray)
     }
+
+    private fun fizzBuzz(array: Array<String>) {
+        for (number in array) {
+
+        }
+    }
+
+
 }
